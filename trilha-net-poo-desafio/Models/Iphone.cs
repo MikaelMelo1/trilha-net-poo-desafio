c46b84ba5ac9b2b6cc7a11fba2ef3823f625a11f
Mikael Melo <85139137+MikaelMelo1@users.noreply.github.com>
2024-05-18 19:37:27 -0300

    Add files via upload

---
+++ trilha-net-poo-desafio/Models/Iphone.cs
@@ -0,0 +1,19 @@
+using System;
+using DesafioPOO.Models;
+
+namespace DesafioPOO.Models
+{
+    public class Iphone : Smartphone
+    {
+
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Iphone...");
+        }
+    }
+}
---
+++ trilha-net-poo-desafio/Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 512);
+Iphone iphone = new Iphone("987654321", "Iphone 12", "987654321", 512);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+
